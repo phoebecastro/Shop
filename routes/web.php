0803249dfb02294068409dd96d10b874e7ce7f74
unknown <pmcastro@gbox.adnu.edu.ph>
2023-04-02 22:01:51 +0800

    comment

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/shop', [ProductController::class, 'index'])->name('shop');
